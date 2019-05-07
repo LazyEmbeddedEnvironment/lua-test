c04c96285cfa8d94bbe7e3e8e15b172fec807446
Jón Bjarni Bjarnason <jbbjarnason@marel.com>
2019-05-07 19:05:24 +0000

    Refactor to class

--- Config.lua
+++ Config.lua
@@ -5,21 +5,22 @@ local rapidjson = require("rapidjson")
 local home = os.getenv ( "HOME" )
 local configRecipe = home.."/"..appBase.frameWorkName.."/config/"..appBase.appName
 local configFilePath = configRecipe.."."..appBase.name..".json"
-local config = nil
-local function openConfigFile(path) return io.open(path, "r") end
-local function exampleConfig() return {} end
-local function tryGetDefaultConfig()
+
+local Config = require('pl.class')() -- Private
+function Config:openConfigFile(path) return io.open(path, "r") end
+function Config:exampleConfig() return {} end
+function Config:tryGetDefaultConfig()
     local defaultConfPath = configRecipe..".json"
-    local file = openConfigFile(defaultConfPath)
+    local file = self:openConfigFile(defaultConfPath)
     if not file then rapidjson.dump(exampleConfig(), defaultConfPath) end
     return rapidjson.load(defaultConfPath) 
 end
-local function generateConfig()
-    local defaultConfig = tryGetDefaultConfig()
+function Config:generateConfig()
+    local defaultConfig = self:tryGetDefaultConfig()
     rapidjson.dump(defaultConfig, configFilePath)
-    return openConfigFile(configFilePath)
+    return self:openConfigFile(configFilePath)
 end
-local function insertNewKeysIfKnown(finalConfigTab, defConfigTab)
+function Config:insertNewKeysIfKnown(finalConfigTab, defConfigTab)
     local ty1 = type(finalConfigTab)
     local ty2 = type(defConfigTab)
     if ty1 ~= ty2 then return false end
@@ -27,27 +28,28 @@ local function insertNewKeysIfKnown(finalConfigTab, defConfigTab)
 
     for k1,v1 in pairs(finalConfigTab) do
         local v2 = defConfigTab[k1]
-        if v2 == nil or not insertNewKeysIfKnown(v1,v2) then return false end
+        if v2 == nil or not self:insertNewKeysIfKnown(v1,v2) then return false end
     end
     for k2,v2 in pairs(defConfigTab) do
         local v1 = finalConfigTab[k2]
         if v1 == nil then finalConfigTab[k2] = v2 end
-        if v1 == nil or not insertNewKeysIfKnown(v1,v2) then return false end
+        if v1 == nil or not self:insertNewKeysIfKnown(v1,v2) then return false end
     end
     return true
 end
-local function checkForChangedDefaultConfFile()
-    local defaultConfig = tryGetDefaultConfig()
-    insertNewKeysIfKnown(config, defaultConfig)
+function Config:checkForChangedDefaultConfFile()
+    local defaultConfig = self:tryGetDefaultConfig()
+    self:insertNewKeysIfKnown(config, defaultConfig)
 end
-local function tryOpenConfig()
-    local file = openConfigFile(configFilePath)
-    local forceReadFile = file and file or generateConfig()
-    config = rapidjson.decode(forceReadFile:read("*all"))
+function Config:tryOpenConfig()
+    local file = self:openConfigFile(configFilePath)
+    local forceReadFile = file and file or self:generateConfig()
+    self.config = rapidjson.decode(forceReadFile:read("*all"))
     forceReadFile:close()
-    checkForChangedDefaultConfFile()
-    rapidjson.dump(config, configFilePath) 
-    return config
+    self:checkForChangedDefaultConfFile()
+    rapidjson.dump(self.config, configFilePath) 
+    return self.config
 end
 
-return tryOpenConfig()
+local private = Config()
+return private:tryOpenConfig()
